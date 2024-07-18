1c6682a6bf02391d94e8fe1a461ea2cede3ab24b
Rashell Aldas <rashellaldas@rashells-mbp.mynetworksettings.com>
2024-07-18 04:02:25 -0400

    for now

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,9 +1,23 @@
 class Quadrilateral {
-
+  constructor(side1, side2, side3, side4) {
+    this.side1 = side1
+    this.side2 = side2
+    this.side3 = side3
+    this.side4 = side4
+  }
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3 + this.side4
+  }
 }
 
-class Rectangle {
-
+class Rectangle extends Quadrilateral {
+  constructor(length, width) {
+    super(length, width, length, width)
+  }
+  getArea(s1, s2) {
+    this.s1 = s1
+    this.s2 = s2
+  }
 }
 
 class Square {
